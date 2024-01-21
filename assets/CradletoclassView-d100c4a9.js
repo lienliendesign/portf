f29c1461446eec1373a904aca63f5722172aa56f
lienliendesign <146346874+lienliendesign@users.noreply.github.com>
2024-01-21 18:01:20 +0100

    Add files via upload

---
+++ assets/CradletoclassView-d100c4a9.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,b as c}from"./index-c117212f.js";const s={},a=c("h1",null,"About page",-1),n=[a];function r(_,l){return o(),t("div",null,n)}const i=e(s,[["render",r]]);export{i as default};
